c4bee18bec8215cc94a3a7fbe44cb39b2cde725e
Tyler Schumacher <tschuma3@uccs.edu>
2023-02-10 18:33:47 -0700

    Implemented some of the data preprocessing for the Seq2Seq. Took some intuition notes

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Deep-Learning-and-NLP-A-Z-How-to-create-a-ChatBot
+
+End-to-end Deep Learning Models
+    -- End-to-end
+        -- One neural network that runs the whole show
+Seq2Seq
+    -- Architecture
+        -- Follows a Recurrent Neural Network
+            -- Utalizing Memeory
+            -- A LSTM layer
+        -- Encoder and Decoder
+        -- Can have multiple hidden layers to put the input through
+    -- Training
+        -- Weights
+            -- Updated normally through backpropigation like a RNN
+            -- Weights are the same for every h(of n) value
+        -- The model will updates weights based on the words that it has previously given
+        -- The goal for the Seq2Seq is that it wants to model the logic and behavior that was observed in the email
+Greedy Decoding vs Beam Search Decoding 
+    -- What are the differnces
+        -- Greedy
+            -- It only looks at the word with the highest probibility to give an output sentence
+        -- Beam
+            -- It looks at the top 3 words with the highest probibility
+            -- The look is like a binary tree
+            -- This is similar to how autocorrect works
+            -- It will throw away the options with the lowest joint probibility
+            -- There are techniques to make the three outputs different
+Attention Mechanisms
+    -- Global attention
+        -- Looking at each of the weights of importance
+        -- The decoder will give weights of importance to each input
+            -- The bigger the weight the higher importance
+            -- Softmax function is applied
+            -- Create a contex vector
+                -- A weighted sum of all the different weights of importance
+    -- Local attention 
+        -- Similar to what humans do
+        -- Only look at some of the weights of importance 
---
+++ chatbot.py
@@ -0,0 +1,101 @@
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+#region Data Preprocessing
+
+#Importing the dataset
+lines = open(r'data\movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n') 
+conversations = open(r'data\movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n') 
+
+#Creating a dictionary that maps each line and its id
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+
+#Creating a list of all the conversations
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+
+#Getting seperately the questions and the answers
+questions = []
+answers = []
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i + 1]])
+
+#Doing the first cleaning of the texts
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", ", ", text)
+    return text
+
+#Cleaning the questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+
+#Cleaning the answers
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean_text(answer))
+
+#Creating a dictionary that maps each word to its number of occurences
+word2count = {}
+for question in clean_questions:
+    for word in question.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+for answer in clean_answers:
+    for word in answer.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+
+#Creating two dictionaries that map the questions words and the answers words to a unique integer --> tokenization
+threshold = 20
+questionswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        questionswords2int[word] = word_number
+        word_number += 1
+answerswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        answerswords2int[word] = word_number
+        word_number += 1
+
+#Adding the last tokens to these two dictionaries
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int) + 1
+for token in tokens:
+    answerswords2int[token] = len(answerswords2int) + 1
+
+#Creating the inverse dictionary of the answerwords2int dictionary
+answersints2word = {w_i: w for w, w_i in answerswords2int.items()}
+
+#endregion
